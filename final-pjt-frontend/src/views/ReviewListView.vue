755393f1fbc592c14d621cccf93fa60853124607
MinjinChae <eominjin25@gamil.com>
2022-06-09 14:41:59 +0900

    Update ReviewList & addReviewBtn

--- final-pjt-frontend/src/views/ReviewListView.vue
+++ final-pjt-frontend/src/views/ReviewListView.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
-    
+    <div class="container my-5 p-5">
+      <router-link :to="{ name: 'reviewnew' }">
+        <button class="addReview m-2">Add Review</button>
+      </router-link>
+      <table class="table align-middle mb-0 bg-white">
+        <thead class="bg-light p-2">
+          <tr>
+            <th>제목</th>
+            <th class="text-center">작성자</th>
+            <th class="text-center">작성 시간</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="review in reviews" :key="review.pk">
+            <td>
+              <router-link :to="{ name: 'reviewdetail', params: { reviewPk: review.id } }">
+                {{ review.title }} 은(는) {{ review.content }} 이다.
+              </router-link>
+            </td>
+            <td class="text-center">{{ review.user.username }}</td>
+            <td class="text-center">{{ $moment(review.created_at).format('YYYY-MM-DD') }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -22,8 +46,11 @@ export default {
 </script>
 
 <style scoped>
- .bigTable {
-   display: flex;
-   justify-content: center;
- }
+  .addReview {
+    background-color: #f3d42a;
+    color: white;
+    padding: 12px;
+    border: none;
+    border-radius: 4px;
+  }
 </style>
