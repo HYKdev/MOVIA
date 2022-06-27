e6f8e23737a6948bb52f6d65065efc24a4f68a8a
MinjinChae <eominjin25@gamil.com>
2022-06-27 10:49:23 +0900

    Update Signup
    template 및 style 작성

--- final-pjt-frontend/src/views/SignupView.vue
+++ final-pjt-frontend/src/views/SignupView.vue
@@ -1,38 +1,30 @@
 <template>
-  <div class="background">
-    <br />
-    <br />
-    <br />
-    <div class="container d-flex justify-content-center">
-      <div class="signupform">
-        <h1>Signup</h1>
-
+  <div class="container">
+    <div class="card">
+      <div class="logoBox">
+        <img src="@/assets/movia_onlyText.png" alt="moviaLogo">
+      </div>
+      <div class="signupForm">
+        <h1>Sign up</h1>
         <account-error-list v-if="authError"></account-error-list>
-
         <form @submit.prevent="signup(credentials)">
-          <div class="form-outline mb-4">
+          <div class="username">
             <label for="username"></label>
             <input v-model="credentials.username" type="text" id="username" placeholder="Username" required />
           </div>
 
-          <div class="form-outline mb-4">
-            <label for="password1"></label>
-            <input v-model="credentials.password1" type="password" id="password1" placeholder="Password" required />
+          <div class="password">
+            <label for="password"></label>
+            <input v-model="credentials.password" type="password" id="password" placeholder="Password" required />
           </div>
 
-          <div class="form-outline mb-4">
+          <div class="password2">
             <label for="password2"></label>
-            <input
-              v-model="credentials.password2"
-              type="password"
-              id="password2"
-              placeholder="Password Confirmation"
-              required
-            />
+            <input type="password" id="password2" placeholder="Password Confirmation" required />
           </div>
 
           <!-- Submit button -->
-          <button type="submit" class="signupBtn btn btn-primary btn-block mb-4">Signup</button>
+          <button type="submit" class="registerBtn">Register</button>
         </form>
       </div>
     </div>
@@ -68,45 +60,74 @@ export default {
 
 <style scoped>
 .container {
-  width: 550px;
-  padding: 50px;
-  border: 1px solid black;
-  border-radius: 5%;
-}
-
-.signupform {
   position: relative;
-  width: 100%;
-  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
 }
 
-h1 {
+.container .card {
+  width: 450px;
+  height: 600px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 20px;
+  box-shadow: 0 25px 40px rgba(0, 0, 0, 0.2);
+}
+
+.container .card .logoBox {
+  width: 450px;
+  height: 250px;
+  background: black;
+  border-radius: 20px 20px 0 0;
+}
+
+.container .card .logoBox img {
+  width: 400px;
+  position: absolute;
+  top: 70px;
+  left: 25px;
+}
+
+.container .card .signupForm {
+  padding: 20px;
+}
+
+.container .card .signupForm h1 {
   color: black;
   font-size: 35px;
-  font-weight: 500px;
-  margin-bottom: 40px;
+  font-weight: 600;
   text-align: center;
+  margin-bottom: 15px;
 }
 
-.form-outline input {
-  width: 100%;
-  height: 40px;
-  padding: 20px;
-  border-radius: 15px;
+.container .card .signupForm form input {
+  width: 350px;
+  height: 35px;
+  border: 1px solid #ced4da;
+  border-radius: 7px;
+  padding: 10px;
+  margin: 12px;
+}
+
+.container .card .signupForm form .registerBtn {
+  width: 350px;
+  height: 35px;
+  background-color: #f3d42a;
   border: none;
-  border: 1px solid rgba(74, 74, 74, 0.5);
+  border-radius: 7px;
+  margin: 10px 10px 20px 10px;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 1.5;
+  cursor: pointer;
+  transition: background-color .15s ease-in-out;
 }
 
-.signupBtn {
-  border-radius: 15px;
-}
-
-hr {
-  height: 2px;
-  color: black;
-}
-/* .background {
-  background-color: white;
-  height: 100vh;
-} */
+.container .card .signupForm form .registerBtn:hover {
+    background-color: rgba(232, 201, 48, 1)
+  }
 </style>
