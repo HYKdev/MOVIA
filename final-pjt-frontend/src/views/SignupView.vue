e8b8b56e8c179aea3e4e8ee7019bcb6e7cad2b64
MinjinChae <eominjin25@gamil.com>
2022-06-30 11:28:30 +0900

    Update Signup
    SignupView.vue 의 template account error list 변경 및 style 수정

--- final-pjt-frontend/src/views/SignupView.vue
+++ final-pjt-frontend/src/views/SignupView.vue
@@ -5,22 +5,37 @@
         <img src="@/assets/movia_onlyText.png" alt="moviaLogo">
       </div>
       <div class="signupForm">
-        <h1>Sign up</h1>
-        <account-error-list v-if="authError"></account-error-list>
         <form @submit.prevent="signup(credentials)">
           <div class="username">
             <label for="username"></label>
             <input v-model="credentials.username" type="text" id="username" placeholder="Username" required />
+            <div v-if="authError">
+              <div v-if="authError.username">
+                <span v-for="error in authError.username" :key="error.id">
+                  <p class="error">{{ error }}</p>
+                </span>
+              </div>
+            </div>
           </div>
 
           <div class="password">
             <label for="password"></label>
             <input v-model="credentials.password1" type="password" id="password1" placeholder="Password" required />
+            <div v-if="authError">
+              <div v-if="authError.password1">
+                <p class="error">{{ authError.password1[0] }}</p>
+              </div>
+            </div>
           </div>
 
           <div class="password2">
             <label for="password2"></label>
             <input v-model="credentials.password2" type="password" id="password2" placeholder="Password Confirmation" required />
+            <div v-if="authError">
+              <div v-if="authError.non_field_errors">
+                <p class="error">비밀번호가 일치하지 않습니다.</p>
+              </div>
+            </div>
           </div>
 
           <!-- Submit button -->
@@ -33,13 +48,10 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
-import AccountErrorList from '@/components/AccountErrorList.vue';
+
 
 export default {
   name: 'SignupView',
-  components: {
-    AccountErrorList,
-  },
   data() {
     return {
       credentials: {
@@ -60,7 +72,6 @@ export default {
 
 <style scoped>
 .container {
-  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -68,32 +79,32 @@ export default {
 }
 
 .container .card {
-  width: 450px;
-  height: 600px;
+  width: 500px;
+  height: 650px;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
   border-radius: 20px;
   box-shadow: 0 25px 40px rgba(0, 0, 0, 0.2);
 }
 
 .container .card .logoBox {
-  width: 450px;
-  height: 250px;
+  width: 100%;
+  height: 100%;
   background: black;
   border-radius: 20px 20px 0 0;
+  text-align: center;
+  line-height: 300px;
 }
 
 .container .card .logoBox img {
-  width: 400px;
-  position: absolute;
-  top: 70px;
-  left: 25px;
+  width: 100%;
+  vertical-align: middle;
 }
 
 .container .card .signupForm {
-  padding: 20px;
+  padding: 40px;
 }
 
 .container .card .signupForm h1 {
@@ -104,9 +115,13 @@ export default {
   margin-bottom: 15px;
 }
 
+ .accountError {
+  font-size: 15px;
+}
+
 .container .card .signupForm form input {
-  width: 350px;
-  height: 35px;
+  width: 400px;
+  height: 40px;
   border: 1px solid #ced4da;
   border-radius: 7px;
   padding: 10px;
@@ -114,8 +129,8 @@ export default {
 }
 
 .container .card .signupForm form .registerBtn {
-  width: 350px;
-  height: 35px;
+  width: 400px;
+  height: 40px;
   background-color: #f3d42a;
   border: none;
   border-radius: 7px;
@@ -130,4 +145,16 @@ export default {
 .container .card .signupForm form .registerBtn:hover {
     background-color: rgba(232, 201, 48, 1)
   }
+
+.error {
+  color: red;
+  font-size: 15px;
+  margin: 0 0 10px 12px;
+}
+
+@media  screen and (max-width:480px) {
+  .container .card {
+    min-width: 435px;
+  }
+}
 </style>
